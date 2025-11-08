df88e04275dde8f332f89f60ebeb8cfc55f4062a
abelmayehchandom <abelmayehchandom@users.noreply.github.com>
2025-11-08 17:19:24 +0000

    Add routes-validator-example.ts, user.styl, utils-advanced-styles.java, vercel.json

---
+++ frontend/web/src/utils-resolver-local/module-ape/utils-advanced-styles.java
@@ -0,0 +1,3 @@
+// Auto-generated file: /Users/kevinguoquan/Desktop/zama备用文件/测试/Zama项目/vestingcloak/frontend/web/src/utils-resolver-local/module-ape/utils-advanced-styles.java
+// Generated at: 2025-11-27T12:25:22.462Z
+
