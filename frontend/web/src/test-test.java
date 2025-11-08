ea5f846ce05b1f33bdb60467716d7749b6a21514
abelmayehchandom <abelmayehchandom@users.noreply.github.com>
2025-11-08 15:17:02 +0000

    Add test-test.java, window.d.ts, email-provider.ts, handler.jsx

---
+++ frontend/web/src/test-test.java
@@ -0,0 +1,3 @@
+// Auto-generated file: /Users/kevinguoquan/Desktop/zama备用文件/测试/Zama项目/vestingcloak/frontend/web/src/test-test.java
+// Generated at: 2025-11-27T12:25:22.464Z
+
---
+++ frontend/web/src/utils-resolver-local/handler.jsx
@@ -0,0 +1,4 @@
+import axios from 'axios';
+
+export const advanced_service_pages = () => { return null; };
+
